1fafb7a83e95835e71b4924e8663712128a4c495
cjgcs <2901055734@qq.com>
2022-03-02 18:25:55 +0800

    1

---
+++ wfd2112/JS/day01/day01_all/02_var.js
@@ -0,0 +1,31 @@
+//声明变量 
+var x=1;
+var y=2;
+var dept='测试部';
+//console.log(x,y,dept);
+//练习：声明多个变量，分别保存一个员工的编号，姓名，性别，生日，工资；最后输出这几项
+var eid=1;
+var ename='涛哥';
+var sex='男';
+var birthday='1973-7-15';
+var salary=50000;
+//console.log(eid,ename,sex,birthday,salary);
+
+var a1=2;
+//var 1a=3;//数字不能出现在开头
+var _a3=5;
+var $_a4=7;
+//var var=8;//不能使用关键字
+var c='北京';//不能用汉字
+//console.log(c);
+
+
+var user;//undefined  未定义 表示空值
+user='tao';
+user='xin';
+user=2;
+//console.log(user);
+
+//一次声明多个变量
+var n1=1,n2=2,n3;
+console.log(n1,n2,n3);
