3e3f3e123b090437299486f4e81b24a86bdace5f
cjgcs <2901055734@qq.com>
2022-03-20 15:00:26 +0800

    1

--- wfd2112/JS/day06/work/04_function.js
+++ wfd2112/JS/day06/work/04_function.js
@@ -1,5 +1,5 @@
 //函数声明
-function fn(){
+/*function fn(){
 
 }
 //匿名函数创建--函数表达式
@@ -10,9 +10,27 @@ var fun=function(){
 //fun();
 console.log(fun);
 console.log(fn);
+console.log(typeof fn);
+*/
 
+//使用匿名函数创建函数getSum，传递任意两个数字，返回两者之间所有整数的和。
+/*
+var getSum=function(n,m){
+ for(var i=n,sum=0;i<=m;i++){
+   sum+=i;
+  
+ } 
+ return sum;
+}
+console.log(getSum(1,100));
+*/
 
-
+console.log(c);//function
+var c=5;//用5覆盖之前保存的函数
+function c(){	//var c=function(){}
+  console.log(2);
+}
+console.log(c);//5
 
 
 
