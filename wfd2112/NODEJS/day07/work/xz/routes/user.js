440dbbec47531b77c322002539e6f85736a2e012
cjgcs <2901055734@qq.com>
2022-04-25 16:55:17 +0800

    1

--- wfd2112/NODEJS/day07/work/xz/routes/user.js
+++ wfd2112/NODEJS/day07/work/xz/routes/user.js
@@ -98,16 +98,16 @@ r.delete('/',(req,res,next)=>{
 });
 
 
-//4.查找用户接口(get /)
-//接口地址：http://127.0.0.1:8080/v1/users/
+//4.查找用户接口(get /info)
+//接口地址：http://127.0.0.1:8080/v1/users/info
 //请求方式：get
 
-r.get('/',(req,res,next)=>{
-  var d=req.body;
+r.get('/info/:uid',(req,res,next)=>{
+  var d=req.params;
   console.log(d);
-  if(!d.uid){
-    res.send({code:401,msg:'用户编号不能为空'});
-  }
+  //if(!d.uid){
+  //  res.send({code:401,msg:'用户编号不能为空'});
+  //}
   pool.query('select * from xz_user where uid=?',[d.uid],(err,result)=>{
     if(err){
 	  next(err);
@@ -122,6 +122,73 @@ r.get('/',(req,res,next)=>{
   });
   
 });
+
+
+
+//练习：编写修改用户数据的接口(put  /info)，传递用户的编号、邮箱、手机、真实姓名、性别，获取传递的参数，验证各项是否为空，执行SQL命令，判断修改是否成功
+
+//5.修改用户接口(put /info)
+//接口地址：http://127.0.0.1:8080/v1/users/info
+//请求方式：put
+
+r.put('/info',(req,res,next)=>{
+	var e=req.body;
+	console.log(e);
+    /*
+	if(!e.uname){
+	  res.send({code:401,msg:'用户名不能为空'});
+	}
+    if(!e.email){
+	  res.send({code:403,msg:'邮箱不能为空'});
+	}
+    if(!e.phone){
+	  res.send({code:404,msg:'电话不能为空'});
+	}
+    if(!e.user_name){
+	  res.send({code:406,msg:'真实姓名不能为空'});
+	}
+    if(!e.gender){
+	  res.send({code:407,msg:'性别不能为空'});
+	}
+	*/
+	var i=400;
+	for(var k in e){
+		i++;
+	  //console.log(k,e[k]);
+	  if(!e[k]){
+	    res.send({code:i,msg:k+'不能为空'});
+	  }
+	}
+
+	//验证手机
+	if(!/^1[3-9]\d{9}$/.test(e.phone)){
+	  res.send({code:407,msg:'手机格式错误'});
+	}
+	
+	//验证邮箱
+	if(!/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(e.email)){
+    res.send({code:408,msg:'邮箱格式错误'});
+	return;
+    }
+	//SQL语句
+	pool.query('update xz_user set? where uid=?',[e,e.uid],(err,result)=>{
+	  if(err){
+	    next(err);
+		return;
+	  }
+	  console.log(result);
+	  if(result.affectedRows===0){
+	    res.send({code:501,msg:'修改失败'});
+	  }else{
+	    res.send({code:200,msg:'修改成功'});
+	  }
+	})
+  
+});
+
+
+
+
 
 
 
